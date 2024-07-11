2d436ee41905bc90c92d2f2fc0bf23542e9f3353
Ladan <ladan.rb@gmail.com>
2024-07-11 11:02:39 +0200

    Added styling to the page.

--- Tic-Tac-Toe/src/components/TicTacToe.tsx
+++ Tic-Tac-Toe/src/components/TicTacToe.tsx
@@ -1,10 +1,17 @@
 import './TicTacToe.css';
+import circle_icon from '../assets/circle.png';
+import cross_icon from '../assets/cross.png'
 
 
 const TicTacToe = () => {
     return (
-        <>
-        </>
+        <div className='container'>
+            <h1 className='title'> Tic Tac Toe Game 
+                in <span> React </span>
+            </h1>
+            <div className='board'></div>
+            <button className='reset'> Reset </button>
+        </div>
     )
 }
 
